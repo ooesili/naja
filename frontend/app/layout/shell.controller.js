38a7c40a5448972cd0ba092472f6d51bb87122b9
Wesley Merkel <ooesili@gmail.com>
2015-05-04 00:51:10 -0600

    Remove duplicate fixture data from ShellController

--- frontend/app/layout/shell.controller.js
+++ frontend/app/layout/shell.controller.js
@@ -13,31 +13,6 @@ function ShellController(jsonData, toolbar) {
   vm.keyDown = keyDown;
   vm.clearObj = clearObj;
   vm.parse = parse;
-  vm.obj = {
-    string: "Hello world!",
-    array: [
-      'Hello',
-      'world!',
-      {
-        arrayObject: {
-          arrObjArr: {
-            oh: 'yeah'
-          }
-        }
-      }
-    ],
-    subObj: {
-      'Hello': 'World!',
-      'Quote': '"Test"',
-      'Backslash': '\\Back\\slash\\'
-    },
-    nestedArrays: [
-      [1,2,3,4],
-      [5,6,7,8],
-      [[9],[10]]
-    ],
-    '<em>injected</em>': '<strong>strong</strong>'
-  };
 
   function keyDown(e) {
     if (jsonData.obj !== undefined) {
