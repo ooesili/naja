52ba30c901afe1230adc7e54612c660a84bc8a14
Wesley Merkel <ooesili@gmail.com>
2015-05-06 19:24:39 -0600

    Add auto-scrolling to the home page's navbar

--- frontend/app/home/home.controller.js
+++ frontend/app/home/home.controller.js
@@ -1,4 +1,8 @@
-function HomeController() {
+import $ from 'jquery';
+import _ from 'lodash';
+
+HomeController.$inject = ['$scope'];
+function HomeController($scope) {
   var vm = this;
   // screenshots
   vm.editImg = require('./screenshots/edit.png');
@@ -6,6 +10,39 @@ function HomeController() {
   vm.hoverImg = require('./screenshots/hover.png');
   vm.keyboardImg = require('./screenshots/keyboard.png');
   vm.toolbarImg = require('./screenshots/toolbar.png');
+  // initialize scrolling
+  var scrollPoints = {
+    navigation: $('.about-3'),
+    intro: $('.about-1'),
+  };
+  vm.scrollOn = null;
+  // scroll event handler
+  var container = $('.home-scroll-base');
+  console.log(container);
+  container.on('scroll', function() {
+    var currentScroll = container.scrollTop();
+    // figure out where we are
+    _.any(scrollPoints, function(elem, name) {
+      return elem.position().top < currentScroll && (vm.scrollOn = name);
+    }) || (vm.scrollOn = null);
+    $scope.$apply();
+  });
+  // "anchor" scrolling
+  vm.scrollTo = scrollTo;
+  return;
+
+  function scrollTo(name) {
+    var position;
+    if (name === 'top') {
+      position = 0;
+    } else {
+      var position = scrollPoints[name].position().top + 5;
+    }
+    // find the current scroll point
+    container.animate({
+      scrollTop: position
+    }, 250);
+  }
 }
 
 export default HomeController;
